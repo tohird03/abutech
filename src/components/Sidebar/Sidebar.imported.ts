96b0b8397207fb002cffba4153b890cf092d480f
tohird03 <tohirjondoniyorov@gmail.com>
2023-04-02 09:29:31 +0500

    create file structure

---
+++ src/components/Sidebar/Sidebar.imported.ts
@@ -0,0 +1,35 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  InboxIcon,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  MailIcon,
+  MenuIcon,
+  Toolbar,
+  Typography,
+};
---
+++ src/interface/sidebar.interface.ts
@@ -0,0 +1,3 @@
+export interface sidebarProps {
+    window?: () => Window;
+}
